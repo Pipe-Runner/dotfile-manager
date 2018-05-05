f27a67993e7955f964af6cc68fbff68ecae0e137
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2018-05-05 11:55:21 +0530

    zipping subroutine added

--- setup.pl
+++ setup.pl
@@ -1,19 +1,22 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+require "./modules/data_backup/Zip_unzip.pl";
 
 print "=========================== Utility 2.0 =============================\n";
 
 # ===========================================================================================================================
 #                                                   User service choice
 # ===========================================================================================================================
-print "Select an option\n
-	1. Backup\n
-	2. Copy Scripts\n
-	3. Restore Data\n
-	4. Restore Scripts
-\n";
-print ">>> ";
-my $user_choice = <STDIN>;
-chomp $user_choice;
-print $user_choice;
+# print "Select an option\n
+# 	1. Backup\n
+# 	2. Copy Scripts\n
+# 	3. Restore Data\n
+# 	4. Restore Scripts
+# \n";
+# print ">>> ";
+# my $user_choice = <STDIN>;
+# chomp($user_choice);
+# print $user_choice;
+
+# Zip_unzip::spit_zip();
