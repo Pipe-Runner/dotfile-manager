8daccf8a5826654f55914f673949077c3a2b3ac7
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-10-09 23:07:15 +0530

    minor changes

--- setup.pl
+++ setup.pl
@@ -4,21 +4,5 @@ use warnings;
 require "./perl_modules/data_backup/Data_backup.pl";
 require "./perl_modules/script_backup/Script_backup.pl";
 
-print "=========================== Utility 2.0 =============================\n";
-
-# ===========================================================================================================================
-#                                                   User service choice
-# ===========================================================================================================================
-# print "Select an option\n
-# 	1. Backup\n
-# 	2. Copy Scripts\n
-# 	3. Restore Data\n
-# 	4. Restore Scripts
-# \n";
-# print ">>> ";
-# my $user_choice = <STDIN>;
-# chomp($user_choice);
-# print $user_choice;
-
 # Data_backup::spit_zip();
 Script_backup::pull();
